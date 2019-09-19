86a8c09f6a367e5b7358243508fac238ffe9a486
darkares23 <desarrollojuseca@gmail.com>
2019-09-19 14:47:55 -0500

    added task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char c = 'a';
+while (c <='z')
+{
+putchar(c);
+c++;
+}
+putchar ("\n");
+return (0);
+}
