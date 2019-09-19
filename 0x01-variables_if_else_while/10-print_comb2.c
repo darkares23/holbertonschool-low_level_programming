364cf214013d4d96d993453cc29a91d25f036668
darkares23 <desarrollojuseca@gmail.com>
2019-09-19 17:08:25 -0500

    added task 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int numA, numB, numC;
+for (numA = 0; numA < 100; numA++)
+{
+numB = numA / 10;
+numC = numA % 10;
+putchar(numB + '0');
+putchar(numC + '0');
+if (numA < 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
