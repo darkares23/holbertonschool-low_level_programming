b5758e918be176293216c7aa745e30a0d0a19d93
darkares23 <desarrollojuseca@gmail.com>
2019-09-19 10:10:21 -0500

    Comment spaces

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: return error 1
- */
+* main - Entry point
+*
+* Return: return error 1
+*/
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
