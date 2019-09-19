fc2866437f73fc3ee4c7cbd9931e35cdfbc4fe3f
darkares23 <desarrollojuseca@gmail.com>
2019-09-19 14:18:05 -0500

    Added task 1: module % 10 to find the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,20 +9,22 @@
 int main(void)
 {
 	int n;
+	int lastDigit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	lastDigit = n % 10;
 	if (n > 5)
 	{
-	printf("Last digit of %d is %d\n and is greater than 5", n, n)
+	printf("Last digit of %d is %d\n and is greater than 5", n, lastDigit);
 	}
 	else if (n == 0)
 	{
-	printf("Last digit of %d is %d\n and is 0", n, n)
+	printf("Last digit of %d is %d\n and is 0", n, lastDigit);
 	}
 	else
 	{
-	printf("Last digit of %d is %d\n and is less than 6 and not 0", n, n)
+	printf("Last digit of %d is %d\n and is less than 6 and not 0", n, n);
 	}
 	return (0);
 }
