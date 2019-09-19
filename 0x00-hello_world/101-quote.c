cf62737bd4723b86cafe11ed8de0557166997d37
darkares23 <desarrollojuseca@gmail.com>
2019-09-19 09:41:02 -0500

    Fixed task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,8 @@
  */
 int main(void)
 {
-write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 1);
+    int a;
+    A = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", a);
 return (1);
 }
