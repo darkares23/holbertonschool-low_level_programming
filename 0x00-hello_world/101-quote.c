bc4f321dc97b866900dbe5227196c47bbd70cbd3
darkares23 <desarrollojuseca@gmail.com>
2019-09-19 09:47:30 -0500

    Fixing space errors

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-    int a;
-    A = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", a);
+int a;
+a = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
+write(2,"and that piece of art is useful - Dora Korpar, 2015-10-19\n", a);
 return (1);
 }
