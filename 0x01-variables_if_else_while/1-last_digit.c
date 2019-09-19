762063b56326153755a6a41f01db9879b7b0d73d
darkares23 <desarrollojuseca@gmail.com>
2019-09-19 14:30:27 -0500

    Fixed if decalration condition variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,11 +14,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastDigit = n % 10;
-	if (n > 5)
+	if (lastDigit > 5)
 	{
 	printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 	}
-	else if (n == 0)
+	else if (lastDigit == 0)
 	{
 	printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 	}
