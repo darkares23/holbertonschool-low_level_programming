6363f9e16b71bdccf05e098a12fc1d7e347fdb87
darkares23 <desarrollojuseca@gmail.com>
2019-09-24 10:56:04 -0500

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,17 +2,15 @@
 /**
 *print_last_digit - Display last digit of a number
 *
-*Return: if is a positive zero or negative
-*
 *@n: number to verify
+*
+*Return: if is a positive zero or negative
 */
 int print_last_digit(int n)
 {
 int lastDigit;
 if (n < 0)
-{
 n = n * -1;
-}
 lastDigit = n % 10;
 _putchar(lastDigit + '0');
 return (lastDigit);
