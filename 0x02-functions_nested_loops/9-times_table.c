f5ab54797ae9f775e79bee8de8a46ce2d3f5b9b8
darkares23 <desarrollojuseca@gmail.com>
2019-09-24 15:29:40 -0500

    Added task 9 fixed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,43 +4,37 @@
 */
 void times_table(void)
 {
-int a, b, c, digit1, digit2;
-
+int a, b, resutl, digit1, digit2;
 for (a = 0; a <= 9; a++)
 {
 for (b = 0; b <= 9; b++)
 {
-c = a * b;
-if (c >= 10)
+resutl = a * b;
+if (resutl >= 10)
 {
-digit1 = c / 10;
-digit2 = c % 10;
-
-_putchar(digit1 + '0');
+digit1 = resutl % 10;
+digit2 = resutl / 10;
+_putchar(' ');
 _putchar(digit2 + '0');
+_putchar(digit1 + '0');
 }
 else
 {
-if (a != 0)
+if (b != 0)
 {
 _putchar(' ');
+_putchar(' ');
 }
-if (a == 0)
-{
-_putchar (' ');
-}
-_putchar(c + '0');
+_putchar(resutl + '0');
 }
 if (b != 9)
 {
 _putchar(',');
-_putchar(' ');
 }
 if (b == 9)
 {
 _putchar('\n');
 }
 }
-b = 0;
 }
 }
