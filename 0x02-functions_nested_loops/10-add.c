c8a2d8a6a1d6aee8e58a0848b7c7cce86951e58a
darkares23 <desarrollojuseca@gmail.com>
2019-09-24 12:54:38 -0500

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+*add - Display last digit of a number
+*
+*@a: number 1
+*
+*@b: number 1
+*
+*Return: adds of a and b
+*/
+int add(int a, int b)
+{
+int result;
+result = a + b;
+return (result);
+}
