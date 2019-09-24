8a590ddc3732c8f85cf3f4e4b46be5afc7a301b2
darkares23 <desarrollojuseca@gmail.com>
2019-09-24 10:50:26 -0500

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,9 @@
 #include "holberton.h"
 /**
 *print_last_digit - Display last digit of a number
+*
 *Return: if is a positive zero or negative
+*
 *@n: number to verify
 */
 int print_last_digit(int n)
