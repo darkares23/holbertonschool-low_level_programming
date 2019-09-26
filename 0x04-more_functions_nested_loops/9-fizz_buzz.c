e08c6e322cf1e0625d8d88073438007a3e4a5037
darkares23 <desarrollojuseca@gmail.com>
2019-09-26 15:35:05 -0500

    task 9 fixed function print spaces

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,14 +11,14 @@ int numbers;
 for (numbers = 1; numbers <= 100; numbers++)
 {
 if (numbers % 3 == 0 && numbers % 5 == 0)
-printf ("%s, ", "FizzBuzz");
+printf("%s, ", "FizzBuzz");
 else if (numbers % 3 == 0)
-printf ("%s ", "Fizz");
+printf("%s ", "Fizz");
 else if (numbers % 5 == 0)
-printf ("%s ", "Buzz");
+printf("%s ", "Buzz");
 else
 {
-printf ("%d ", numbers);
+printf("%d ", numbers);
 }
 }
 }
