cf5129e2974a9bb4554ef4d2e7c9762580e48430
darkares23 <desarrollojuseca@gmail.com>
2019-09-26 16:00:01 -0500

    Fixed 9 FILE

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - print if is 3 Fizz 5 Buzz or FizzBuzz.
- *
- * Return: Always 0.
- */
+*print_diagonal - prints a diagonal line
+*Return: 14 lines of numbers
+*
+*@n: Large of the line
+*/
 int main(void)
 {
-int numbers;
-for (numbers = 1; numbers <= 100; numbers++)
+int number;
+for (number = 1; number <= 100; number++)
 {
-if (numbers % 3 == 0 && numbers % 5 == 0)
-printf("%s ", "FizzBuzz");
-else if (numbers == 100)
+if ((number % 3) == 0 && (number % 5) != 0)
+printf("Fizz ");
+else if (number == 100)
 printf("Buzz");
-else if (numbers % 3 == 0)
-printf("%s ", "Fizz");
-else if (numbers % 5 == 0)
-printf("%s ", "Buzz");
+else if ((number % 5) == 0 && (number % 3) != 0)
+printf("Buzz ");
+else if ((number % 3) == 0 && (number % 3) == 0)
+printf("FizzBuzz ");
 else
-{
-printf("%d ", numbers);
-}
+printf("%d ", number);
 }
 printf("\n");
+return (0);
 }
