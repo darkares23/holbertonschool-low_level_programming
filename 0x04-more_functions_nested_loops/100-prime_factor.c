8cfd43e8b1757607849170119531cdba13bc4885
darkares23 <desarrollojuseca@gmail.com>
2019-09-26 16:52:22 -0500

    Task 100 prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - prints prime factor
+ *
+ * Return: 0;
+ */
+int main(void)
+{
+long i, primeFactor;
+long number;
+number = 612852475143;
+for (i = 2; i < sqrt(number); i++)
+{
+if ((number % i) == 0)
+{
+primeFactor = i;
+number = number / i;
+i = 1;
+}
+}
+primeFactor = number;
+printf("%ld\n", primeFactor);
+return (0);
+}
