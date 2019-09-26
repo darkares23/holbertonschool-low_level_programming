70d959413901978d586d32aeac269d363470ab59
darkares23 <desarrollojuseca@gmail.com>
2019-09-26 15:03:53 -0500

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+*print_square - prints squares equal to the size variable
+*Return: 14 lines of numbers
+*
+*@size: Large of the line
+*/
+void print_square(int size)
+{
+int lines, square;
+if (size <= 0)
+_putchar('\n');
+for (lines = 0; lines < size; lines++)
+{
+for (square = 0; square < size; square++)
+{
+if (size != 0)
+_putchar ('#');
+}
+_putchar ('\n');
+}
+}
+
