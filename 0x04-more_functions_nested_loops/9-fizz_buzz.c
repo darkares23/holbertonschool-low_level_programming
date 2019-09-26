3357673aae7bd24dd4092dbee82ca2f0b5d79ffd
darkares23 <desarrollojuseca@gmail.com>
2019-09-26 15:33:56 -0500

    task 9: print FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print if is 3 Fizz 5 Buzz or FizzBuzz.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int numbers;
+for (numbers = 1; numbers <= 100; numbers++)
+{
+if (numbers % 3 == 0 && numbers % 5 == 0)
+printf ("%s, ", "FizzBuzz");
+else if (numbers % 3 == 0)
+printf ("%s ", "Fizz");
+else if (numbers % 5 == 0)
+printf ("%s ", "Buzz");
+else
+{
+printf ("%d ", numbers);
+}
+}
+}
