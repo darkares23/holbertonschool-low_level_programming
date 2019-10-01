4e029746e4d54002fca1ffc106689e6103941059
darkares23 <desarrollojuseca@gmail.com>
2019-10-01 22:23:34 +0000

    added task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+  *puts_half -  print hal string
+  *@str: string to print half
+*/
+
+void puts_half(char *str)
+{
+int i = 0, i2 = 0;
+char text;
+	for (; str[i] != '\0'; i++)
+	{
+	}
+	i2 = (i + 1) / 2;
+	for (; str[i2] != '\0'; i2++)
+	{
+		text = str[i2];
+		_putchar(text);
+	}
+	_putchar('\n');
+}
