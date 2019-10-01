30c53fb186056f49a6bca5e5a91504d2e58e6143
darkares23 <desarrollojuseca@gmail.com>
2019-10-01 20:30:45 +0000

    task 4: print string rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+  *print_rev - prints reverse string
+  *@s: string to print
+  *
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	char text;
+
+	for (; s[i] != '\0'; i++)
+	{
+
+	}
+	for (i = i; i > 0; i--)
+	{
+		text = s[i];
+		_putchar(text);
+	}
+}
