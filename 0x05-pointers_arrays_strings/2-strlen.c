c6eebc0cc8910f1a6a19f2782d346f7a12aa6366
darkares23 <desarrollojuseca@gmail.com>
2019-10-01 18:30:28 +0000

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *_strlen - return lenght of s char
+ *@s: string to be sized
+ *Return: return lenght
+ */
+int _strlen(char *s)
+{	int string;
+	for(string = 0; s[string] != 0; string++)
+	{
+	}
+	return (string);
+}
