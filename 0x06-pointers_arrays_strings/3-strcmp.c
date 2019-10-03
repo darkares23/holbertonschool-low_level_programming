bf371d6d9f9942d5884d87b7cfe6b927d21cce18
darkares23 <desarrollojuseca@gmail.com>
2019-10-03 22:00:57 +0000

    added task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string one to compare
+ * @s2: string two to compare
+ * Return: an integer less than 0 if s1 is found
+ */
+
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0')
+	{
+		if (s1 > s2)
+		{
+			return (s1[i] - s2[i]);
+		}
+		if (s2[i] > s1[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+		return (0);
+}
