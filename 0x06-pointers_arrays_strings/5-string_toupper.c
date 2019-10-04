ffc8879ecad52fd802d45666a2924f6198b0390d
darkares23 <desarrollojuseca@gmail.com>
2019-10-03 23:04:14 -0500

    added task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - changes all to upper
+ * @c: character to change
+ * Return: character
+ */
+
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+			c[i] = c[i] - 32;
+	}
+	return (c);
+}
