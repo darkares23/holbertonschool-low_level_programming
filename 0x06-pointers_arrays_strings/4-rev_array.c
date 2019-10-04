3ce8cf8e6cff99e536ecb4fd3a29b6ae7a259680
darkares23 <desarrollojuseca@gmail.com>
2019-10-03 22:31:45 -0500

    added task 4 fixed

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,24 +1,25 @@
 #include "holberton.h"
 
+/**
+ * reverse_array - reverse string
+ * @a: string array
+ * @n: position of element
+ */
+
 void reverse_array(int *a, int n)
 {
-	int i = 0, rev;
-	char strg;
+	int i = 0;
+	int temp;
 
-	for (; i < n && a[i] != '\0'; i++)
+	n = n - 1;
+
+	while (i < n)
 	{
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
+		i++;
+		n--;
 	}
-
-	rev = 0;
-	i = i - 1;
-
-	while (rev < i)
-	{
-		strg = a[i];
-		a[i] = a[rev];
-		a[rev] = strg;
-		rev++;
-		i--;
-	}	
 
 }
