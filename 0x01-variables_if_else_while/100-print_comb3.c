103a8b9f688f61b635dc3bf35b89a18517a7ef18
darkares23 <desarrollojuseca@gmail.com>
2019-10-05 15:12:31 -0500

    task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,34 +9,21 @@ int main(void)
 int a = 0, b = 1, c = 0;
 for (; c >= 0 && c <= 99; c++)
 {
-a = c / 10;
-b = c % 10;
+    a = c / 10;
+    b = c % 10;
 
-if (a < b)
-{
-if (c <= 9)
-{
-putchar(a + '0');
-putchar(b + '0');
-if (c != 99)
-{
-putchar(',');
-putchar(' ');
-}
-}
-if (c >= 10)
-{
-a = c / 10;
-b = c % 10;
-putchar(a + '0');
-putchar(b + '0');
-if (c != 99)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
+    if (a < b && c != 99)
+        {
+            a = c / 10;
+            b = c % 10;
+            putchar(a + '0');
+            putchar(b + '0');
+            if (a == 9 && b == 8)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
 }
 putchar('\n');
 return (0);
