5c709a5cdc77f72d41068a00787da57d20e2eaa0
darkares23 <desarrollojuseca@gmail.com>
2019-10-06 12:24:24 -0500

    added task 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
 
 /**
-*
+* main - Return print number
+* Return: always 0
 */
 
 int main(void)
 {
-    int d1, d2, d3;
+int d1, d2, d3;
 
-        for (d1 = 48; d1 < 58; d1++)
-        {
-            for (d2 = d1 + 1; d2 < 58; d2++)
-            {
-                for (d3 = d2 + 1; d3 < 58; d3++)
-                {
-                    putchar(d1);
-                    putchar(d2);
-                    putchar(d3);
-                    
-                    if(d1 == 55 && d2 == 56 && d3 == 57)
-                    {
-                    }
-                    else
-                    {
-                        putchar(',');
-                        putchar(' ');
-                    }
-                    
-                }   
-            }
-        }
-        putchar('\n');
+for (d1 = 48; d1 < 58; d1++)
+{
+for (d2 = d1 + 1; d2 < 58; d2++)
+{
+for (d3 = d2 + 1; d3 < 58; d3++)
+{
+putchar(d1);
+putchar(d2);
+putchar(d3);
 
-    return (0);
+if (d1 == 55 && d2 == 56 && d3 == 57)
+{
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+
+return (0);
 }
