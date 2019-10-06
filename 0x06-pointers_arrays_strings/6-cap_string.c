ed6a1e4ead242f54078cc95324c1c76b928e74ff
darkares23 <desarrollojuseca@gmail.com>
2019-10-06 16:50:36 -0500

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,27 +1,27 @@
 #include "holberton.h"
 /**
-* cap_string - capitalizes all words of a string.
-* @n : pointer
-* Return: Always n
+* cap_string - first letter capitaliced
+* @s : parameter
+* Return: changed string
 **/
 
 char *cap_string(char *s)
 {
-	int a;
+int a;
 
-	for (a = 0; s[a] != '\0'; a++)
-	{
-		if (s[a] >= 'a' && s[a] <= 'z')
-		{
-			if (s[a - 1] == ' ' || s[a - 1] == '.' || s[a - 1] == '\n' || s[a - 1] == 9)
-			{
-				if (s[a - 1] == 9)
-				{
-					s[a - 1] = ' ';
-				}
-				s[a] = s[a] - 32;
-			}
-		}
-	}
-	return (s);
+for (a = 0; s[a] != '\0'; a++)
+{
+if (s[a] >= 'a' && s[a] <= 'z')
+{
+if (s[a - 1] == ' ' || s[a - 1] == '.' || s[a - 1] == '\n' || s[a - 1] == 9)
+{
+if (s[a - 1] == 9)
+{
+s[a - 1] = ' ';
+}
+s[a] = s[a] - 32;
+}
+}
+}
+return (s);
 }
