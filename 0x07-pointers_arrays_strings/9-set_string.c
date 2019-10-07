f8654534a611f9659f4980fe8d4c50a0de6fb15b
darkares23 <desarrollojuseca@gmail.com>
2019-10-07 18:31:04 -0500

    Added task 8 with name 9: asigned pointer to a char

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+*set_string - set a pointer to a char
+*@s: pointer to a pointer
+*@to: char to save
+*Return: always 0
+*/
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -5,3 +5,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
+void set_string(char **s, char *to);
