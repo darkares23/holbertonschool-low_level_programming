022fdad979f9c4ff50f0d10bea849065baf25be7
darkares23 <desarrollojuseca@gmail.com>
2019-10-07 13:24:34 -0500

    Add task 1: Copy a block of memory to another pointer

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *_memcpy - Function to copy a block o memory
+ *@dest: Destiantion of copy memory
+ *@src: Source of memory to copy
+ *@n: array size.
+ *Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int count;
+	count = 0;
+
+	for (src[count] = 0; src[count] != '\0' && count < n; count++)
+		dest[count] = src[count];
+	return (dest);
+}
