d93330eba6afc96708ce9e47468a6530409c1473
darkares23 <desarrollojuseca@gmail.com>
2019-10-07 17:42:22 -0500

    Added task 4: print ocurrency in two string

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,13 +11,18 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int i = 0, j;
+	int bolean;
 
 		for (i = 0; s[i] != '\0'; i++)
 		{
 			for (j = 0; accept[j] != '\0'; j++)
 			{
 			if (s[i] == accept[j])
-				return (s + 1);
+				bolean = 1;
+			}
+			if (bolean == 1)
+			{
+				return (s + bolean);
 			}
 		}
 	return (NULL);
