85fa00ae73e6dd5d01e0248884a0a517187c834c
darkares23 <desarrollojuseca@gmail.com>
2019-10-09 16:10:49 +0000

    task 1: print reverted string usig recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* _print_rev_recursion - print a string from a pointer
+*@s: string to print.
+*Return: reverse string
+*/
+
+void _print_rev_recursion(char *s)
+{
+if (!*s) /* this means s pointer diferent to the null position */
+{
+return;
+}
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
