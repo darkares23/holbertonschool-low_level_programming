d1eda89fc790554972aa3d14c7226c1c0970c9ef
darkares23 <desarrollojuseca@gmail.com>
2019-10-09 17:27:30 +0000

    task 2: print lenght of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+*_strlen_recursion - return length of the string
+*@s: vstring to cout size
+*Return: length
+*/
+int _strlen_recursion(char *s)
+{
+if (!*s)
+return (0);
+return (1 + _strlen_recursion(s + 1));
+}
