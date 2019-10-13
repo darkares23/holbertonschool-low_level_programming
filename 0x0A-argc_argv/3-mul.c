8e362b18afe402c810bdb3238be91e83e981ec90
darkares23 <desarrollojuseca@gmail.com>
2019-10-13 16:37:00 -0500

    added task 3 correction

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,20 +11,19 @@
 
 int main(int argc, char **argv)
 {
-int num1, num2;
-int total;
+	int num1, num2, mul;
 
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-else
-{
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-total = num1 * num2;
-printf("%d\n", total);
-}
-return (0);
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		mul = num1 * num2;
+		printf("%d\n", mul);
+	}
+	return (0);
 }
