66f1f13c69cd920d2b4ea3ea3c45fb1d6af20191
darkares23 <desarrollojuseca@gmail.com>
2019-10-13 15:27:10 -0500

    betty style correction task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 /**
- */
+*main - prints is own name
+*@argc: number of parameters
+*@argv: array of parameters in comand line
+*Return: always 0
+*/
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
 	if (argc == 1)
 		printf("%s\n", argv[0]);
