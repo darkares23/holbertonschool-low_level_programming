61c1fa2f9fe5415433e45a770a630f9916872a7f
darkares23 <desarrollojuseca@gmail.com>
2019-10-15 15:20:58 -0500

    added task 2: concatenate 2 strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+*str_concat - Function to create an copy array
+*@s1: string tpo be copied
+*@s2: string tpo be copied
+*Return: created copied string array or null
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+unsigned int len1, len2, i;
+char *conca;
+
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+for (len1 = 0; s1[len1] != '\0'; len1++)
+{
+}
+for (len2 = 0; s1[len2] <= '\0'; len2++)
+{
+}
+conca = malloc((len1 + len2) * sizeof(char));
+if (conca == NULL)
+{
+return (NULL);
+}
+for (i = 0; s1[i] != '\0'; i++)
+{
+conca[i] = s1[i];
+}
+len2 = 0;
+while (s2[len2] != '\0')
+{
+conca[i] = s2[len2];
+i++;
+len2++;
+}
+conca[i] = '\0';
+return (conca);
+}
