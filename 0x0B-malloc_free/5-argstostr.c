8d87cdfc3b9fccf3441c11b3e0a1af1fa60416e0
Holberton <holberton@Holbertons-iMac-7.local>
2019-10-16 17:14:57 -0500

    added start of task 5

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * argstostr -  frees a 2 dimensional grid
+ * previously created by your alloc_grid function.
+ * @ac:  grid
+ * @av: height of grid
+ * Return: 2 dimensional array of integers or null
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int count1, count2, count3;
+	char *ptr;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (count1 = 0; count1 < ac; count1++)
+	{
+		for (count2 = 0; av[count2] != '\0' ; count2++)
+		{
+			count3++;
+		}
+		count2 = 0;
+	}
+	ptr = malloc((count3 + ac) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+
+	count3 = 0;
+
+	for (count1 = 0; count1 < ac; count1++)
+	{
+		for (count2 = 0; av[count2] != '\0' ; count2++)
+		{
+			ptr[count3] = av[count1][count2];
+			count3++;
+			count2++;
+		}
+		count2 = 0;
+	}
+	return (ptr);
+}
