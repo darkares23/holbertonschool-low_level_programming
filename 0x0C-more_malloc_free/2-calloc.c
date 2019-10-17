5d4a3798e3cfec0faaa5b19371d3123f1b52aaff
Holberton <holberton@Holbertons-iMac-7.local>
2019-10-17 16:04:06 -0500

    task2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,13 +12,20 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
+	unsigned int i, sized;
 
+	sized = nmemb * size;
+	
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	
-	ptr = malloc(nmemb * size);
+	ptr = malloc(sized);
 	if (ptr == NULL)
 		return (NULL);
+	for (i = 0; i < sized; i++)
+	{
+		ptr[i] = 0;
+	}
 
 	return (ptr);
 }
