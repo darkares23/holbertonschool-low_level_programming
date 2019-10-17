dc2e59a91d98d18f4e3f7550b16b9f7ec7136df8
Holberton <holberton@Holbertons-iMac-7.local>
2019-10-17 12:59:51 -0500

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates mem
+ * @b: amount of int space requiered
+ * Return: Null or space required
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *space;
+
+	if (!(b > 0))
+		exit(98);
+
+	space = malloc(b);
+
+	if (space == NULL)
+		exit(98);
+
+	return (space);
+
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,6 @@
+**More malloc and free**
+
+*This project will help to understand what are the functions malloc and realloc and the better the use of them*
+*and how to use the exit() function*
+
+*0-malloc_checked.c : allocate memory using malloc*
