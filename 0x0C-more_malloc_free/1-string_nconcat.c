0f497a745cb32ae5d84e805f7ca743677f708143
Holberton <holberton@Holbertons-iMac-7.local>
2019-10-17 14:23:48 -0500

    Fixed task 1 and aupdated reamde file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,42 +1,33 @@
-#include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * string_nconcat - concatenate 2 strings in a pointer
- * @s1: first string
- * @s2: second string
- * @n: number of character for 2 string to copy
- * Return: Null or space required
+ * string_nconcat - concatenates two strings
+ * @s1:stirng 1
+ * @s2:string 2
+ * @n: n bytesofstring 2
+ * Return: pointer
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int count1, count2;
-char *conca, *empty_string;
+	char *ptr, *empt;
+	unsigned int i, k;
 
-empty_string = "";
-
-if (s1 == NULL)
-	s1 = empty_string;
-if (s2 == NULL)
-	s2 = empty_string;
-
-for (count1 = 0; s1[count1] != '\0'; count1++)
-	;
-conca = malloc((count1 + (n * sizeof(*s2) + 1) * sizeof(*conca)));
-
-if (conca == NULL)
-	return (NULL);
-
-for (count1 = 0; s1[count1] != '\0'; count1++)
-	conca[count1] = s1[count1];
-
-for (count2 = 0; s2[count2] != '\0' && count2 <= n; count1++, count2++)
-	conca[count1] = s2[count2];
-
-conca[count1] = '\0';
-
-return (conca);
-
+	empt = "";
+	if (s1 == NULL)
+		s1 = empt;
+	if (s2 == NULL)
+		s2 = empt;
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	ptr = malloc((i + (n * sizeof(*s2) + 1) * sizeof(*ptr)));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		ptr[i] = s1[i];
+	for (k = 0; s2[k] != '\0' && k < n; k++, i++)
+		ptr[i] = s2[k];
+	ptr[i] = '\0';
+	return (ptr);
 }
