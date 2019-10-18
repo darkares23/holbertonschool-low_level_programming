688f3d77f0a732fd33c55cbb46179ae05ee1f369
darkares23 <desarrollojuseca@gmail.com>
2019-10-17 22:11:14 -0500

    fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,54 +11,36 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *r_ptr;
-
-	if (ptr == NULL)
-	{
-		r_ptr = malloc(new_size);
-		if (r_ptr == NULL)
-		{
-			free(ptr);
-			return (NULL);
-		}
-		free(ptr);
-		return (r_ptr);
-	}
-	if (new_size == old_size)
-		return (ptr);
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	r_ptr = malloc(new_size);
-	if (r_ptr == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	if (new_size > old_size)
-		_memcpy(r_ptr, ptr, old_size);
-	free(ptr);
-	return (r_ptr);
-}
-
-/**
- * _memcpy - copies n bytes form the memory area src
- * to memory area dest
- * @src:
- * @dest:
- * @n:  bytes
- * Return: dest
- */
-
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
+	char *memory, *temp, *ar;
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
-	{
-		dest[i] = src[i];
-	}
-	return (dest);
+if (ptr == NULL)
+{
+ar = malloc(new_size);
+if (ptr == NULL)
+{
+free(ptr);
+return (NULL);
+}
+free(ptr);
+return (ar);
+}
+if (new_size == old_size)
+return (ptr);
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+memory = malloc(new_size);
+temp = ptr;
+
+if (new_size > old_size)
+old_size = new_size;
+
+for (i = 0; i < old_size; i++)
+memory[i] = temp[i];
+
+free(ptr);
+return (temp);
 }
