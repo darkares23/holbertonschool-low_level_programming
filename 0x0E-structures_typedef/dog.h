0292a792a108fbc227ea4c0952d95ac887a46d77
Holberton <holberton@Holbertons-iMac-7.local>
2019-10-21 14:06:21 -0500

    Added task 0 correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,17 @@
 #ifndef DOG
 #define DOG
-
+/**
+ * struct dog - new structure of a dog
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: owner name
+*/
 
 struct dog
 {
-char *name;
-float age;
-char * owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif
