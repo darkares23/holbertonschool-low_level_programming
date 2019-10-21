c527353bad599783eb3ff754aac4c211a9bcb59d
Holberton <holberton@Holbertons-iMac-7.local>
2019-10-21 16:27:13 -0500

    Added missing outputs

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,10 +13,37 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *newDog;
+	char *new_name, *new_owner;
+	int i, j, copy;
 
 	newDog = malloc(sizeof(struct dog));
 	if (newDog == NULL)
 		return (NULL);
+	for (i = 0; i < name[i]; i++)
+	;
+	for (j = 0; j < owner[j]; j++)
+	;
+	new_name = malloc(i * sizeof(char));
+	if (new_name == NULL)
+	{
+		return (NULL);
+		free(name);
+	}
+	new_owner = malloc(j * sizeof(char));
+	if (new_name == NULL)
+	{
+		return (NULL);
+		free(owner);
+	}
+	for (copy = 0; copy <= i; copy++)
+	{
+		new_name[copy] = name[copy];
+	}
+	for (copy = 0; copy <= i; copy++)
+	{
+		new_owner[copy] = owner[copy];
+	}
+
 	newDog->name = name;
 	newDog->age = age;
 	newDog->owner = owner;
