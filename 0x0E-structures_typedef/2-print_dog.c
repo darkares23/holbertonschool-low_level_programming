0f7809d707ddbf90b3c8faa92dc5128d03779b0b
Holberton <holberton@Holbertons-iMac-7.local>
2019-10-21 15:06:32 -0500

    added task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,13 +5,10 @@
 /**
 * print_dog - function to init a variable of type dog
 * @d: variable to init
-* @name: dogs name
-* @age: dogs age
-* @owner: owner name
 */
 
 void print_dog(struct dog *d)
-{	
+{
 	if (d == NULL)
 		;
 	if (d->name == NULL)
@@ -19,6 +16,5 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);	
-	
+	printf("Owner: %s\n", d->owner);
 }
