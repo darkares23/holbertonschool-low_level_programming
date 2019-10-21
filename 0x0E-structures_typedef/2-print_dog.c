fdbf57cd08f99fdb8dc946cd0f3ce4505fd3d77c
Holberton <holberton@Holbertons-iMac-7.local>
2019-10-21 15:11:13 -0500

    New sintaxis for task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,12 +9,16 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		;
-	if (d->name == NULL)
-		printf("Name: (nil)\n");
-	else
-		printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		if (d->owner == NULL)
+			printf("Age: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
 }
