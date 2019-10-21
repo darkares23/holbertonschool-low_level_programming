28bd726f48284901f3cd9f6d07fa817e343189d6
Holberton <holberton@Holbertons-iMac-7.local>
2019-10-21 14:21:34 -0500

    task 1: initializevariable

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -15,9 +15,9 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		;
 	else
 	{
-		d -> name = name;
-		d -> age = age;
-		d -> owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
-	
+
 }
