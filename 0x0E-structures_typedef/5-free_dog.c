258af209c9f83b6e64dc2fbc832c5306425c9ad0
Holberton <holberton@Holbertons-iMac-7.local>
2019-10-21 16:11:26 -0500

    Added task 5 free space

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -9,9 +9,11 @@
 
 void free_dog(dog_t *d)
 {
-	if (d != NULL)
-	{
+	if (d == NULL)
+		return;
+	if (d->name != NULL)
 		free(d->name);
+	if	(d->owner != NULL)
 		free(d->owner);
-		free(d);
+	free(d);
 }
