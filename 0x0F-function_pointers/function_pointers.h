6c322949b6b29d77daf4de050227a5d4936a2c35
Holberton <holberton@Holbertons-iMac-7.local>
2019-10-23 12:25:47 -0500

    added header fixed

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
-#include <stdalign.h>
-#include <stdio.h>
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
+#include <stdalign.h>
+#include <stdio.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
