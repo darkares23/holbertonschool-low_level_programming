6de04b0572684924c4e6462b9fc43cce31076538
Holberton <holberton@Holbertons-iMac-7.local>
2019-10-25 13:28:41 -0500

    task 2 printing strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - function to sum all args
+ * @n: const number of arguements
+ * @separator: separator
+ * RETURN: int sum of all args or null
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list str_list;
+	unsigned int arg_count;
+	char *str;
+
+	if (separator == NULL)
+		exit(98);
+
+	va_start(str_list, n);
+
+	for (arg_count = 0; arg_count < n; arg_count++)
+	{
+		str = va_arg(str_list,  char *);
+		if (str == NULL)
+		{
+			str = "(nil)";
+			printf("%s", str);
+		}
+		else if (arg_count == 0)
+			printf("%s%s", str, separator);
+		else
+			printf("%s", str);
+	}
+	printf("\n");
+	va_end(str_list);
+}
