7d1cabf1507e9fe50982d1902806edc1820794c4
darkares23 <desarrollojuseca@gmail.com>
2019-10-26 13:42:21 -0500

    added fixed task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -71,7 +71,7 @@ void print_all(const char * const format, ...)
 	va_start(list, format);
 	i = 0;
 
-	while (format[i] != '\0' && format != NULL)
+	while (format[i] != '\0')
 	{
 		j = 0;
 		while (j < 4)
