e3c23d11c81f7d6e48e86a5129ef5c7a3bd2e47d
darkares23 <desarrollojuseca@gmail.com>
2019-10-26 16:47:26 -0500

    fixed header description

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,11 +4,6 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-/**
-*struct format_t - Struct format_t
-*@fm: format
-*@f: associated function
-*/
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
@@ -16,6 +11,11 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+*struct format_t - Struct format_t
+*@fm: format
+*@func: associated function
+*/
 typedef struct format_t
 {
 	char *fm;
