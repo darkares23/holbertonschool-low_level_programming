0562368b4566872be347b4e18a0dd7bce17138d7
darkares23 <desarrollojuseca@gmail.com>
2019-10-31 22:28:25 -0500

    added advanced task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,5 +6,6 @@ void exe_before (void) __attribute__ ((constructor));
 */
 void exe_before(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
