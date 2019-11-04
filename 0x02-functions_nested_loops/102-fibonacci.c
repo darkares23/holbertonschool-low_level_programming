51f0ee712c0deb1b77d409f8181e89c79ac7ebf2
darkares23 <desarrollojuseca@gmail.com>
2019-11-03 21:38:33 -0500

    102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,14 +9,18 @@
 int main(void)
 {
 	int i = 0;
-	unsigned int temp = 0, num1 = 1, num2 = 2;
+	unsigned long temp = 0, num1 = 1, num2 = 2;
 
-	for (; i < 50; i++)
+	printf("%ld, %ld", num1, num2);
+
+	for (; i < 48; i++)
 	{
 		temp = num1 + num2;
-		printf("%i\n", temp);
+		printf("%ld", temp);
 		num1 = num2;
 		num2 = temp;
+		if (i != 47)
+			printf(", ");
 	}
 	return (0);
 }
