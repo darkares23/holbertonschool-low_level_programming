100559d8569c9f74e4496aad3934d853bcdbbae1
darkares23 <desarrollojuseca@gmail.com>
2019-11-03 21:45:04 -0500

    102

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - prints fibo acumm less than 4000000
+*Return: always 0
+*/
+int main(void)
+{
+	int i = 0;
+	unsigned long temp = 0, num1 = 1, num2 = 2, acum;
+
+	printf("%ld, %ld, ", num1, num2);
+
+	for (; acum < 4000000; i++)
+	{
+		temp = num1 + num2;
+		if (temp % 2 == 0)
+			acum += temp;
+		num1 = num2;
+		num2 = temp;
+	}
+	printf("%ld\n", acum);
+	return (0);
+}
