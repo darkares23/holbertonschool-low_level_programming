20bafefb41533da3f0ed65ac93c606fa2153388a
darkares23 <desarrollojuseca@gmail.com>
2019-11-03 21:10:28 -0500

    task n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
 *print_times_table - Display the 9 times table
-*@n = times to print
+*@n: times to print
 */
 void print_times_table(int n)
 {
@@ -15,34 +15,32 @@ void print_times_table(int n)
 			digit1 = result % 10;
 			digit2 = (result / 10) % 10;
 			mediumdigit = (result / 100) % 100;
-		if (result > 9 && result < 100)
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(digit2 + '0');
-			_putchar(digit1 + '0');
-		}
-		else if (result > 100)
-		{
-			_putchar(' ');
-			_putchar(mediumdigit + '0');
-			_putchar(digit2 + '0');
-			_putchar(digit1 + '0');
-		}
-		else
-		{
-			if (b != 0)
+			if ((a == 0 && b == 0) || (a != 0 && b == 0))
+				_putchar(digit2 + '0');
+			else if (result < 10)
 			{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(result + '0');
 			}
-			_putchar(result + '0');
-		}
-		if (b != n)
-			_putchar(',');
-		if (b == n)
-			_putchar('\n');
-		}
+			if (result > 9 && result < 100)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(digit2 + '0');
+				_putchar(digit1 + '0');
+			}
+			else if (result >= 100)
+			{
+				_putchar(' ');
+				_putchar(mediumdigit + '0');
+				_putchar(digit2 + '0');
+				_putchar(digit1 + '0');
+			}
+			if (b < n)
+				_putchar(',');
+			}
+		_putchar('\n');
 	}
 }
