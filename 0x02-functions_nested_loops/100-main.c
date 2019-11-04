480c6e04fec8a2fe3f1f6637c9f0f9958b67054e
darkares23 <desarrollojuseca@gmail.com>
2019-11-03 20:59:33 -0500

    task n times table

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -11,8 +11,8 @@ int main(void)
     _putchar('\n');
     print_times_table(5);
     _putchar('\n');
-   
+    print_times_table(98);
     _putchar('\n');
-  
+    print_times_table(12);  
     return (0);
 }
--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,41 +1,48 @@
 #include "holberton.h"
 /**
 *print_times_table - Display the 9 times table
+*@n = times to print
 */
 void print_times_table(int n)
 {
-int a, b, resutl, digit1, digit2;
+	int a, b, result, digit1, digit2, mediumdigit;
 
-for (a = 0; a <= 9; a++)
-{
-    for (b = 0; b <= 9; b++)
-    {
-        resutl = a * b;
-        if (resutl >= 10)
-        {
-            digit1 = resutl % 10;
-            digit2 = resutl / 10;
-            _putchar(' ');
-            _putchar(digit2 + '0');
-            _putchar(digit1 + '0');
-        }
-        else
-        {
-            if (b != 0)
-            {
-                _putchar(' ');
-                _putchar(' ');
-            }
-        _putchar(resutl + '0');
-        }
-        if (b != 9)
-        {
-            _putchar(',');
-        }
-        if (b == 9)
-        {
-            _putchar('\n');
-        }
-    }
-}
+	for (a = 0; a <= n; a++)
+	{
+		for (b = 0; b <= n; b++)
+		{
+			result = a * b;
+			digit1 = result % 10;
+			digit2 = (result / 10) % 10;
+			mediumdigit = (result / 100) % 100;
+		if (result > 9 && result < 100)
+		{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(digit2 + '0');
+			_putchar(digit1 + '0');
+		}
+		else if (result > 100)
+		{
+			_putchar(' ');
+			_putchar(mediumdigit + '0');
+			_putchar(digit2 + '0');
+			_putchar(digit1 + '0');
+		}
+		else
+		{
+			if (b != 0)
+			{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(' ');
+			}
+			_putchar(result + '0');
+		}
+		if (b != n)
+			_putchar(',');
+		if (b == n)
+			_putchar('\n');
+		}
+	}
 }
