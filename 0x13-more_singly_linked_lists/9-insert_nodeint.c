7e38d69d7e9bcc7030fd840598d52dafa9c5aaa3
Juan Ocampo <desarrollojuseca@gmail.com>
2019-11-05 13:04:17 -0500

    task 9 added cases index 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,21 +3,49 @@
 /**
  * insert_nodeint_at_index - prints the size of linked list and the str
  * @head: list_t pointer variable
+ * @idx: index of the node
+ * @n: constant to copy in the new node
  * Return: new node
 */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	listint_t *tmp = *head;
+	listint_t *new_node, *tmp = *head;
 
-	while (tmp)
+	if (head == NULL)
+		return (NULL);
+
+	if (idx == 0)
 	{
-		if (i == idx)
+		new_node = malloc(sizeof(listint_t));
+		if (new_node == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		new_node->n = n;
+		new_node->next = tmp;
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (tmp != NULL)
+	{
+		if (i + 1 == idx)
+		{
+			new_node = malloc(sizeof(listint_t));
+			if (new_node == NULL)
 			{
-				tmp = tmp->n;
-				tmp = tmp->next;
+				free(new_node);
+				return (NULL);
 			}
+			new_node->n = n;
+			new_node->next = tmp->next;
+			tmp->next = new_node;
+			return (new_node);
+		}
+		tmp = tmp->next;
 		i++;
 	}
 	return (0);
