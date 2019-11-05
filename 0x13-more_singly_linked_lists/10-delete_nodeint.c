7679925f73d958d16c620a1928d4e1dd1e6ba5bc
Juan Ocampo <desarrollojuseca@gmail.com>
2019-11-05 14:16:03 -0500

    task 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - prints the size of linked list and the str
+ * @head: list_t pointer variable
+ * @index: index of the node
+ * Return: new node
+*/
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *new_node, *tmp = *head;
+
+	if (head == NULL)
+		return (-1);
+
+	if (index == 0 && (*head)->next != NULL)
+	{
+		*head = (*head)->next;
+		free(tmp);
+		return (1);
+	}
+
+	while (tmp != NULL)
+	{
+		if (i + 1 == index)
+		{
+			new_node = tmp->next;
+			tmp->next = new_node->next;
+			free(new_node);
+			return (1);
+		}
+		tmp = tmp->next;
+		i++;
+	}
+	return (-1);
+}
