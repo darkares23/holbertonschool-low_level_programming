97a169d55897ba49009d6973e6f62e1cd704dbdd
darkares23 <desarrollojuseca@gmail.com>
2019-11-05 21:16:52 -0500

    task 12

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - reverse a llinked list
+ * @head: list_t pointer variable
+ * Return: new node
+*/
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count;
+
+	if (head == NULL)
+		exit(98);
+
+	while (head != NULL && head > head->next)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		count++;
+	}
+	if (head != NULL)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		printf("->[%p] %d\n", (void *)head->next, head->next->n);
+		count++;
+	}
+	return (count);
+}
