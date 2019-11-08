154aef5ae0858200b17903b0dd9e54e86d6d744c
darkares23 <desarrollojuseca@gmail.com>
2019-11-07 19:53:44 -0500

    changed betty task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,10 +11,10 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int count = 0, x_or = n ^ m;
 
-	while (x_or < 0)
+	while (x_or > 0)
 	{
 		if (1 == (x_or & 1))
-			count++;
+			
 		x_or >>= 1;
 	}
 
