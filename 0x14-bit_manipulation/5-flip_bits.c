d4813f9a0b1fe3ba4e930bea71f8ae56d4f5d331
darkares23 <desarrollojuseca@gmail.com>
2019-11-07 19:06:03 -0500

    task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * flip_bits -   function that sets the value of a bit to 0 at a given index.
+ * @n: num
+ * @m: num2
+ * Return: bits or 0
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int count = 1, x_or = n ^ m;
+
+	while (x_or > 0)
+	{
+		if (1 == (x_or & 1))
+			count++;
+		x_or >>= 1;
+	}
+
+	return (count);
+}
