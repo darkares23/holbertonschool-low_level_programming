9c32d9ef49ba9cb41de9e21e3ab62da8bb7305c4
darkares23 <desarrollojuseca@gmail.com>
2019-11-12 17:45:43 -0500

    close changed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fd < 0)
-		return (0);
+		return (-1);
 
 	writ = write(fd, text_content, write_count);
 
