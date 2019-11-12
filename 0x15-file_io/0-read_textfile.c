8877cc38baaede36a67ee86435f48bc23c7f2dfe
Juan Ocampo <desarrollojuseca@gmail.com>
2019-11-12 13:45:55 -0500

    read a file and return the bits readed

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ *read_textfile - function that reads a text file
+ *and prints it to the POSIX standard output.
+ *@filename: file name
+ *@letters: len of the text in the file
+ *Return: actual number of letters it could read and print
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, countRead;
+	char *buff;
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0 || filename == NULL)
+		return (0);
+	buff = malloc(sizeof(char *) * letters);
+
+	if (buff == NULL)
+		return (0);
+	countRead = read(fd, buff, letters);
+
+	write(1, buff, countRead);
+
+	free(buff);
+
+	close(fd);
+	return (countRead);
+}
