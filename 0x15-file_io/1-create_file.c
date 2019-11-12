fec7ee0446320a8c292a3bc1a505af5da81ea4e5
darkares23 <desarrollojuseca@gmail.com>
2019-11-12 17:41:09 -0500

    task 1 fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,8 +11,6 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, write_count = 0, writ;
 
-	if (text_content == '\0')
-		return (-1);
 	if (text_content == NULL)
 		text_content = "";
 	if (filename == NULL)
@@ -21,12 +19,16 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[write_count] != '\0')
 		write_count++;
 
-	fd = open(filename, O_CREAT, O_TRUNC, O_RDWR, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fd < 0)
 		return (0);
 
-	writ = write(STDOUT_FILENO, text_content, write_count);
+	writ = write(fd, text_content, write_count);
 
-	close(fd);
+	if (writ == -1)
+		return (-1);
+
+	if (close(fd) == -1)
+		return (0);
 	return (1);
 }
