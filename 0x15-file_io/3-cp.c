1ee0b48765c5e7087d8c98e382552768d3463138
darkares23 <desarrollojuseca@gmail.com>
2019-11-12 20:01:55 -0500

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,27 +8,27 @@
 */
 int main(int ac, char **av)
 {
-	int fd, fd1, reader = 1024, writer = 0, closer;
-	char *copy[1024];
+	int fd, fd1, buff_len = 1024, writer = 0, closer;
+	char buff[1024];
 	mode_t permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: %s %s %s\n", av[0], av[1], av[2]), exit(97);
-	fd = open(av[1], O_RDONLY, 0664);
+	fd = open(av[1], O_RDONLY);
 	if (fd == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-	fd1 = open(av[2], O_CREAT | O_WRONLY | O_APPEND | O_TRUNC, permissions);
+	fd1 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, permissions);
 	if (fd1 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-	while (reader == 1024)
+	while (buff_len == 1024)
 	{
-		reader = read(fd, copy, 1024);
-		if (reader == -1)
+		buff_len = read(fd, buff, 1024);
+		if (buff_len == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
-		writer = write(fd1, copy, reader);
+		writer = write(fd1, buff, buff_len);
 		if (writer == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
