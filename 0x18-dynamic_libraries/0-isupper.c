c54d26515b1ba51ec255cfb7295fbfa90c67dac8
darkares23 <desarrollojuseca@gmail.com>
2019-12-16 12:23:06 -0500

    added task 1 and correction of prototypes

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+*_isupper - Display message
+*
+*Return: true or false if is uppercase
+*
+*@c: parameter to verify
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+{
+return (0);
+}
+}
