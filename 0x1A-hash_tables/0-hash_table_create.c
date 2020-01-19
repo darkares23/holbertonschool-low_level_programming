a522a9e605d58076b57740b307f5baeba1bf750f
darkares23 <desarrollojuseca@gmail.com>
2020-01-19 15:16:14 -0500

    task 5 fixed blank spaces and betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,23 +9,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_hash_t = malloc(sizeof(hash_table_t));
 	unsigned long int i = 0;
 
-	if (!new_hash_t)
-		return (NULL);
-	if (size <= 0)
+	if (!new_hash_t || size <= 0)
 		return (NULL);
 
 	new_hash_t->array = malloc(sizeof(hash_node_t *) * size);
 	if (!new_hash_t->array)
-	{
-		free(new_hash_t);
 		return (NULL);
-	}
 	while (i < size)
 	{
 		new_hash_t->array[i] = NULL;
 		i++;
 	}
 	new_hash_t->size = size;
-
 	return (new_hash_t);
 }
