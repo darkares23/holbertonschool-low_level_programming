9d987e825b1bb6eb9129af24ec0e91bd83344028
darkares23 <desarrollojuseca@gmail.com>
2020-01-19 13:35:36 -0500

    added task 2 index key

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 /**
- *  hash_table_create - creates a hash table.
+ * hash_table_create - creates a hash table.
  * @size: Size of the array
  * Return: new hash table
  */
--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,12 +1,14 @@
 #include "hash_tables.h"
 /**
- * key_index - gives you the index of a key.
- * @key: is the key
- * @size: size of the array of the hash table
- * Return:  the index at which the key/value pair 
- * should be stored in the array of the hash table
- */
+* key_index - gives you the index of a key.
+* @key: is the key
+* @size: size of the array of the hash table
+* Return:  the index at which the key/value pair
+* should be stored in the array of the hash table
+*/
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	hash_djb2(size);
+	unsigned long int ht = hash_djb2(key);
+
+	return (ht % size);
 }
