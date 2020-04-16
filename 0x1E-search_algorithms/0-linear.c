4bd85525568cc9d18ae2e06fb8324476472fbd58
darkares23 <desarrollojuseca@gmail.com>
2020-04-16 10:26:32 -0500

    added task 0, linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+/**
+ * linear_search - Linear search function
+ * @array: list to serach target
+ * @size: siza of array
+ * @value: target to be found
+ * Return: value ondex in the list
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (!array)
+		return (-1);
+	while (i < size)
+	{
+		printf("Value checked array[%li] = [%i]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH
+#define SEARCH
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif // MACRO
+
