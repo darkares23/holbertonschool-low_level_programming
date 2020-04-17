a161fae8a83e8ed37945924bb38fdb5538ad8cbc
darkares23 <desarrollojuseca@gmail.com>
2020-04-17 10:40:20 -0500

    added test anwers bigO

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+/**
+ * jump_search - 
+ * @array: list to serach target
+ * @size: siza of array
+ * @value: target to be found
+ * Return: value ondex in the list
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+
+}
